b38694cc759123705386c17f4278d6a58fdc7fa7
Levadna Inga <levadnainga@gmail.com>
2026-04-19 22:20:16 +0300

    new project

--- Animator.js
+++ Animator.js
@@ -5,10 +5,8 @@ export class Animator {
     constructor(element) {
         this.element = element;
     }
-
     moveAnimation(name) {
         let style = document.createElement('style');
-
         style.textContent = `
     @keyframes ${name} {
         0%   { transform: translate(0px, 0px); }
@@ -17,23 +15,18 @@ export class Animator {
         75%  { transform: translate(50px, 30px); }
         100% { transform: translate(0px, 0px); }
     }`;
-
         document.head.appendChild(style);
 
         this.element.style.transformBox = 'fill-box';
         this.element.style.transformOrigin = 'center';
-
         this.element.style.animation = `${name} 4s infinite`;
     }
-
     start() {
         this.element.style.animationPlayState = 'running';
     }
-
     pause() {
         this.element.style.animationPlayState = 'paused';
     }
-
     changeStyle(prop, value) {
         this.element.style[prop] = value;
     }
