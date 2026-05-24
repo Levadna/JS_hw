ac904ccce3c56d2ea4b502f3268b93794159afec
Levadna Inga <levadnainga@gmail.com>
2026-05-24 23:45:34 +0300

    new project

--- script.js
+++ script.js
@@ -1,146 +1,120 @@
-//1
-// let field = document.querySelector("#field");
-// let ball = document.querySelector("#ball");
-// field.addEventListener("click", function(event){
-//     let x = event.clientX - 50;
-//     let y = event.clientY - 50;
-//     if(x < 0){
-//         x = 0;
-//     }
-//     if(y < 0){
-//         y = 0;
-//     }
-//     if(x > field.clientWidth - 100){
-//         x = field.clientWidth - 100;
-//     }
-//     if(y > field.clientHeight - 100){
-//         y = field.clientHeight - 100;
-//     }
-//     ball.style.left = x + "px";
-//     ball.style.top = y + "px";
-// })
+document.addEventListener("DOMContentLoaded", function(){
+    let playersDiv = document.querySelector("#players");
+    let teamDiv = document.querySelector("#team");
+    let message = document.querySelector("#message");
+    let clearBtn = document.querySelector("#clearBtn");
+    let saveBtn = document.querySelector("#saveBtn");
+    let team = [];
 
+    fetch("players.json")
+    .then(response => response.json())
+    .then(data => {
+        console.log(data);
+        data.forEach(player => {
+            let card = document.createElement("div");
+            card.className = "card";
+            card.draggable = true;
+            card.id = player.id;
+            card.innerHTML = `
+            <img src="${player.image}">
+            <div class="info">
+                <h2>${player.name}</h2>
+                <p><b>Class:</b> ${player.class}</p>
+                <p><b>Level:</b> ${player.level}</p>
+                <div class="skills">
+                    <b>Skills:</b><br>
+                    ${player.skills.join(", ")}
+                </div>
+                <div class="stats">
+                    <p>Strength: ${player.stats.strength}</p>
+                    <p>Agility: ${player.stats.agility}</p>
+                    <p>Intelligence: ${player.stats.intelligence}</p>
+                </div>
+            </div>
+            `;
+            card.addEventListener("dragstart", function(event){
+                event.dataTransfer.setData("application/json", JSON.stringify(player));
+            });
+            playersDiv.appendChild(card);
+        });
+    });
 
-//2
-// let input = document.querySelector("#name");
-// let message = document.querySelector("#message");
-// input.addEventListener("input", function(){
-//     input.value = input.value.replace(/[0-9]/g, "");
-//     input.value = input.value.replace(/[!@#$%^?&*=+"'`;:<>,/|()]/g, "");
-//     if(input.value.length < 3)
-//     {
-//         message.innerHTML = "Мінімум 3 символи";
-//     }
-//     else if(input.value.length > 20){
-//         input.value = input.value.slice(0,20);
-//         message.innerHTML = "Максимум 20 символів";
-//     }
-//     else{
-//         message.innerHTML = "";
-//     }
-// })
+    teamDiv.addEventListener("dragover", function(event){
+        event.preventDefault();
+        teamDiv.classList.add("hovered");
+    });
 
+    teamDiv.addEventListener("dragleave", function(){
+        teamDiv.classList.remove("hovered");
+    });
 
-//3
-// let titles = document.querySelectorAll("h3");
-// let texts = document.querySelectorAll("p");
-// titles.forEach(function(title){
-//     title.addEventListener("click", function(){
-//         texts.forEach(function(text){
-//             text.style.display = "none";
-//         })
-//         let p = title.nextElementSibling;
-//         p.style.display = "block";
-//     })
-// })
+    teamDiv.addEventListener("drop", function(event){
+        event.preventDefault();
+        teamDiv.classList.remove("hovered");
+        let data = event.dataTransfer.getData("application/json");
+        let player = JSON.parse(data);
+        message.textContent = "";
+        if(team.length >= 3)
+        {
+            message.textContent = "Team is full!";
+            alert("Team is full!");
+            return;
+        }
+        let exists = team.find(p => p.id === player.id);
+        if(exists)
+        {
+            message.textContent = "This character is already in team!";
+            alert("This character is already in team!");
+            return;
+        }
+        if( !player.skills.includes("Magic") && !player.skills.includes("Leadership"))
+        {
+            message.textContent = "Character needs Magic or Leadership skill!";
+            alert("Character needs Magic or Leadership skill!");
+            return;
+        }
+        team.push(player);
+        renderTeam();
+    });
 
+    function renderTeam()
+    {
+        let title = document.createElement("div");
+        title.style.width = "100%";
+        teamDiv.appendChild(title);
+        teamDiv.innerHTML = "<h2>Team</h2>";
+        team.forEach(player => {
+            let div = document.createElement("div");
+            div.className = "teamCard";
+            div.innerHTML = `
+            <img src="${player.image}">
+            <h3>${player.name}</h3>
+            <p>Level: ${player.level}</p>
+            <p>${player.skills.join(", ")}</p>
+            `;
+            div.addEventListener("dblclick", function(){
+                team = team.filter(p => p.id !== player.id);
+                renderTeam();
+            });
+            
+            teamDiv.appendChild(div);
+        });
+    }
+    saveBtn.addEventListener("click", function(){
+        localStorage.setItem("team", JSON.stringify(team));
+        message.textContent = "Team saved!";
+    });
+    clearBtn.addEventListener("click", function(){
+        team = [];
+        localStorage.removeItem("team");
+        renderTeam();
+        message.textContent = "Team cleared!";
+    });
 
-//4
-// let button = document.querySelector("#up");
-// window.addEventListener("scroll", function(){
-//     if(window.scrollY > 100){
-//         button.style.display = "block";
-//     }
-//     else{
-//         button.style.display = "none";
-//     }
-// })
-// button.addEventListener("click", function(){
-//     window.scrollTo(0,0);
-// })
-
-
-//5
-// let div = document.querySelector("#text");
-// let textarea = null;
-
-// document.addEventListener("keydown", function(event){
-//     if(event.ctrlKey && event.key == "e")
-//     {
-//         event.preventDefault();
-//         textarea = document.createElement("textarea");
-//         textarea.value = div.innerHTML;
-//         div.replaceWith(textarea);
-//     }
-//     if(event.ctrlKey && event.key == "s")
-//     {
-//         event.preventDefault();
-//         if(textarea)
-//         {
-//             div.innerHTML = textarea.value;
-//             textarea.replaceWith(div);
-//         }
-//     }
-// })
-
-
-//6
-// let td = document.querySelectorAll("td");
-
-// td.forEach(function(item){
-//     item.addEventListener("mouseover", function()
-//     {
-//         item.style.backgroundColor = "orange";
-//     })
-//     item.addEventListener("mouseout", function(){
-//         item.style.backgroundColor = "blanchedalmond";
-//     })
-// })
-
-
-//7
-let countries = [
-    "Ukraine",
-    "Poland",
-    "Germany",
-    "France",
-    "Italy",
-    "Spain",
-    "Portugal",
-    "Canada",
-    "USA",
-    "Japan",
-    "China",
-    "Brazil"
-];
-let input = document.querySelector("#country");
-let list = document.querySelector("#list");
-
-input.addEventListener("input", function()
-{
-    list.innerHTML = "";
-    let value = input.value.toLowerCase();
-    let result = countries.filter(function(country){
-        return country.toLowerCase().startsWith(value);
-    })
-    result.slice(0,10).forEach(function(country){
-        let div = document.createElement("div");
-        div.classList.add("item");
-        div.innerHTML = country;
-        list.append(div);
-        div.addEventListener("click", function(){
-            input.value = country;
-            list.innerHTML = "";
-        })
-    })
-})
+    let savedTeam = localStorage.getItem("team");
+    if(savedTeam)
+    {
+        team = JSON.parse(savedTeam);
+        renderTeam();
+    }
+});
